a35c01f3371d49bd92485689f10cabd7720d6be4
Letícia Zalasik <leticiazalasik@gmail.com>
2024-11-13 10:04:10 -0300

    :sparkles: Feat: Mudança para que na média apareça apenas duas casas decimais, função para encontrar o aluno com a maior média e função para criar uma lista com os alunos aprovados.

--- arrayGestaoEstudantes.js
+++ arrayGestaoEstudantes.js
@@ -8,12 +8,47 @@ const estudantes = [
 // 1. Calcule a média de cada estudante
 estudantes.forEach(estudante => {
     
-    estudante.media = estudante.notas.reduce((media, nota) => media + nota, 0)/estudante.notas.length;
+    estudante.media = parseFloat((estudante.notas.reduce((media, nota) => media + nota,0)/estudante.notas.length).toFixed(2));
 
 });
-
+console.log(estudantes);
 // 2. Encontre o estudante com a maior média
 
+function buscarMaiorMedia(estudantes){
+let nomeMaiorMedia = " ";
+let notaMaiorMedia = 0;
+
+estudantes.forEach(estudante => {
+    if (estudante.media>notaMaiorMedia) {
+        notaMaiorMedia=estudante.media;
+        nomeMaiorMedia=estudante.nome;
+    }
+    
+});
+
+console.log(`Estudante com a maior média: ${nomeMaiorMedia} - Média: ${notaMaiorMedia} `)
+}
+
+buscarMaiorMedia(estudantes);
+
 // 3. Liste apenas os estudantes aprovados (média >= 7)
+function listarAprovados(estudantes){
+const estudantesAprovados = [];
+
+estudantes.forEach(estudante => {
+    if (estudante.media>=7) {
+        estudantesAprovados.push(estudante);
+    }
+    
+});
+
+console.log(`Lista dos estudantes aprovados:`)
+
+estudantesAprovados.forEach(estudante => {
+console.log(`Nome: ${estudante.nome} ${estudante.media}`)
+});
+}
+
+listarAprovados(estudantes);
 
 // 4. Adicione uma nova nota para cada estudante
